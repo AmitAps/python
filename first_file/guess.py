8eb3c498e9b866fa640ebf3662d2e067d829c1dd
AMIT PRATAP SINGH <47079294+AmitAps@users.noreply.github.com>
2019-02-05 10:57:15 -0500

    Delete guess.py

--- first_file/guess.py
+++
@@ -1,8 +0,0 @@
-print("Welcome to the gussing game")
-g = input("Guess a number betwween 1 to 6 ")
-guess = int(g)
-if guess == 5:
-    print("Congrats you won!!!")
-else:
-    print("Sorry, You loose. Try again..")
-print("Game over. Thank you")
